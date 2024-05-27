36a30994e05c0bd7f7225db143dd50852e68972a
saichethanr <saichethan9019@gmail.com>
2024-05-27 17:45:40 +0530

    some updates

--- frontend/src/components/VideoComponent.jsx
+++ frontend/src/components/VideoComponent.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import '../css/App.css'
 
+
 function VideoStream1() {
   const [counter, setCounter] = useState(0);
 
